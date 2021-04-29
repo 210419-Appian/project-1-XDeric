f3775866199458643db4d040a218ab3b941e82aa
Eric <eeeric3995@yahoo.com>
2021-04-29 16:49:52 -0400

    conforming models to java bean style

--- src/main/java/com/revature/models/AccountType.java
+++ src/main/java/com/revature/models/AccountType.java
@@ -11,4 +11,32 @@ public class AccountType { // checking or savings
 		this.type = type;
 	}
 
+	public AccountType() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountType [typeId=" + typeId + ", type=" + type + "]";
+	}
+	
+	
+
 }
--- src/main/java/com/revature/models/Role.java
+++ src/main/java/com/revature/models/Role.java
@@ -4,4 +4,37 @@ public class Role { // Admin,Employee,Standard,Premium
 	
 	private int roleId;
 	private String role;
+	
+	public Role(int roleId, String role) {
+		super();
+		this.roleId = roleId;
+		this.role = role;
+	}
+	
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [roleId=" + roleId + ", role=" + role + "]";
+	}
+	
 }
