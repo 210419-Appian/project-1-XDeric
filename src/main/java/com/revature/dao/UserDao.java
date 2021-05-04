b11e3544918b5a8391c9f5163a78a8ad0339682a
Eric <eeeric3995@yahoo.com>
2021-05-04 14:21:18 -0400

    maded changes in DAO package

--- src/main/java/com/revature/dao/UserDao.java
+++ src/main/java/com/revature/dao/UserDao.java
@@ -1,5 +1,16 @@
 package com.revature.dao;
 
-public class UserDao {
+import java.util.List;
 
+import com.revature.models.User;
+
+public interface UserDAO {
+	
+	//users
+	public boolean addUser(User user);
+	public User getUser(int id);
+	//employee
+	public List<User> getAllUsers();
+	//Admin
+	public boolean deleteUser(int id);
 }
