58a095a3436e27697fee381fd803672f53b64848
Eric <eeeric3995@yahoo.com>
2021-05-06 19:43:11 -0400

    nothing

--- src/main/java/com/revature/dao/AccountDAOImpl.java
+++ src/main/java/com/revature/dao/AccountDAOImpl.java
@@ -6,19 +6,16 @@ public class AccountDAOImpl implements AccountDAO{
 
 	@Override
 	public int getBalance(int id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public String getStatus() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public AccountType getType() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
